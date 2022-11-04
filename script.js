9d7b7c8b929244d0f8833070d9811a9467667421
bogdankokunin <bkokunin@gmail.com>
2022-11-04 16:17:11 +0300

    Add index.html script.js style.css with main functionality

---
+++ script.js
@@ -0,0 +1,126 @@
+const library = document.querySelector('main');
+const addBtn = document.getElementById('addBtn');
+const addBtnDiv = document.getElementById('addBtnDiv');
+
+let myLibrary = [];
+
+render();
+createForm();
+ 
+function createForm() {
+    const form = document.createElement('form');
+    const authorInput = document.createElement('input');
+    const titleInput = document.createElement('input');
+    const pagesInput = document.createElement('input');
+    const submitBtn = document.createElement('button');
+
+    addBtn.addEventListener('click', (event) => {
+        submitBtn.innerText = 'Add book';
+        
+        form.setAttribute('action', 'post')
+        form.setAttribute('class', 'form');
+        authorInput.setAttribute('type', 'text');
+        titleInput.setAttribute('type', 'text');
+        pagesInput.setAttribute('type', 'number');
+    
+        authorInput.setAttribute('placeholder', 'Author');
+        titleInput.setAttribute('placeholder', 'Title');
+    
+        library.insertBefore(form, addBtnDiv);
+        form.appendChild(authorInput);
+        form.appendChild(titleInput);
+        form.appendChild(pagesInput);
+        form.appendChild(submitBtn);
+    });
+
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (authorInput.value !== '' && titleInput.value !== '' && pagesInput.value !== '') {
+            addBookToLibrary(authorInput.value, titleInput.value, pagesInput.value);
+            render();
+            authorInput.value = ''; 
+            titleInput.value = '';
+            pagesInput.value = '';
+            form.remove();
+        }
+    });
+
+}
+
+function book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(author, title, pages, read) {
+    const newBook = new book(author, title, pages, read);
+    myLibrary.push(newBook);
+}
+
+function render() {
+    if (myLibrary.length > 0) {
+        for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+            createBook(myLibrary[i]);
+        }
+    }
+    
+    else {
+        for (let i = 0; i < myLibrary.length; i++) {
+            createBook(myLibrary[i]);
+        }
+    }
+    
+}
+
+function createBook(item) {
+    const cardDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const buttonsDiv = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+
+    cardDiv.classList.add('book');
+    buttonsDiv.classList.add('buttonsDiv');
+    removeBtn.classList.add('bookButton');
+    readBtn.classList.add('bookButton');
+
+    titleDiv.textContent = item.title;
+    cardDiv.appendChild(titleDiv);
+
+    authorDiv.textContent = `Author: ${item.author}`;
+    cardDiv.appendChild(authorDiv);
+
+    pagesDiv.textContent = `Pages: ${item.pages}`;
+    cardDiv.appendChild(pagesDiv);
+
+    removeBtn.innerText = 'Remove';
+    readBtn.innerText = 'Read';
+
+    removeBtn.addEventListener('click', () => {
+        cardDiv.remove();
+        myLibrary.splice(-1);
+    })
+
+    readBtn.addEventListener('click', () => {
+        if (item.read !== true) {
+            item.read = true;
+            cardDiv.setAttribute('class', 'bookRead');
+            readBtn.innerText = 'Unread';
+        }
+        else if (item.read === true) {
+            item.read = false;
+            cardDiv.setAttribute('class', 'book')
+            readBtn.innerText = 'Read';
+        }
+    })
+
+    cardDiv.appendChild(buttonsDiv);
+    buttonsDiv.appendChild(removeBtn);
+    buttonsDiv.appendChild(readBtn);
+    library.insertBefore(cardDiv, addBtnDiv);
+}
